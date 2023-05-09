ddacd0dffe539c928b05067054c6696aed4483e1
satyam gupta <stymngrya@gmail.com>
2023-05-09 15:49:31 +0530

    payment checkout fixed

--- frontend/src/Pages/Chekout/Checkout.jsx
+++ frontend/src/Pages/Chekout/Checkout.jsx
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from "react";
 import "./Checkout.css";
-import { Box, Flex, Select, useDisclosure } from "@chakra-ui/react";
-import { MdDelete } from "react-icons/md";
+import {
+  Box,
+  Button,
+  CheckboxIcon,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Select,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { MdCheckCircleOutline, MdDelete, MdLocalOffer } from "react-icons/md";
 import axios from "axios";
 import { GET_ALL_CART_ITEMS, DELETE_CART_ITEM } from "../../Utils/url.js";
 
 const Checkout = () => {
   const [cartItems, setCartItems] = useState([]);
+  //   const [totalAmount, setTotalAmount] = useState(0);
   let quantities = [1, 2, 3, 4];
   //----## getting cart items of user----///####-----
   const hbToken = JSON.parse(localStorage.getItem("hbToken"));
@@ -21,7 +33,7 @@ const Checkout = () => {
         })
         .then((res) => {
           setCartItems(res.data.cart);
-          console.log(cartItems);
+          console.log(res.data.cart);
         })
         .catch((err) => {
           console.log(err);
@@ -29,6 +41,14 @@ const Checkout = () => {
     };
     getcartItems();
   }, [hbToken]);
+
+  //   --------------------set Total ammount-----------//
+
+  let totalAmount = cartItems.reduce((acc, item) => {
+    return acc + item.price;
+  }, 0);
+
+  console.log(totalAmount, "ta");
 
   // -----handle delete---####////
 
@@ -57,15 +77,15 @@ const Checkout = () => {
         w={"90%"}
         h={"auto"}
         m={"auto"}
-        border={"2px solid red"}
-        p={"20px"}
+        border={"0px solid red"}
+        p={"10px"}
         justifyContent={"space-between"}
       >
         <Box
           m={"auto"}
-          width={{ base: "90%", sm: "90%", md: "50%" }}
+          width={{ base: "95%", sm: "90%", md: "50%" }}
           h={"auto"}
-          border={"2px solid blue"}
+          border={"0px solid blue"}
         >
           {cartItems.map((item, index) => (
             <Box
@@ -80,10 +100,9 @@ const Checkout = () => {
             >
               <Flex h={"70%"} border={""}>
                 <img
+                  className="card_img"
                   src={item.product_details.api_featured_image}
                   alt="product_image"
-                  width={"100px"}
-                  height={"80px"}
                 />
 
                 <Flex
@@ -122,8 +141,8 @@ const Checkout = () => {
                     border={"0px solid red"}
                     size={"xsm"}
                   >
-                    {quantities.map((qty) => (
-                      <option outline={"0"} value={qty}>
+                    {quantities.map((qty, index) => (
+                      <option key={index} outline={"0"} value={qty}>
                         {qty}
                       </option>
                     ))}
@@ -137,7 +156,66 @@ const Checkout = () => {
             </Box>
           ))}
         </Box>
-        <Box w={"40%"} h={"500px"} border={"1px solid green"}></Box>
+        <Box className="payment_box" w={{ base: "90%", md: "50%" }} h={"500px"}>
+          <Box className="payment_inner_div">
+            <div>
+              <h2>PAYMENT-DETAILS</h2>
+            </div>
+            <Box  border={'0px solid red'} flexDirection={{base:"column",md:"row" }} display={{  md: "flex" }} className="coupon">
+              <InputGroup outline={"none"} w={"85%"}>
+                <InputLeftElement
+                  pointerEvents="none"
+                  color="gray.300"
+                  fontSize="1.2em"
+                  children=<MdLocalOffer />
+                />
+                <Input placeholder="Enter Promo Code" />
+                <InputRightElement
+                  children={<MdCheckCircleOutline color="green.500" />}
+                />
+              </InputGroup>
+              <Button mt={{base:'10px' ,md:"0"}} w={{ base: "80px" ,md:"100px" }} size={"sm"} colorScheme="green">
+                Apply
+              </Button>
+            </Box>
+            <div className="payment">
+              <div>
+                {" "}
+                <h1>Payment</h1>{" "}
+              </div>
+              <div>
+                {" "}
+                <div>Total MRP</div> <div>{totalAmount.toFixed(2)}</div>{" "}
+              </div>
+              <div>
+                {" "}
+                <div>Discount (20%)</div>{" "}
+                <div>
+                  {" "}
+                  <span className="redspan">
+                    -{((totalAmount * 20) / 100).toFixed(2)}
+                  </span>
+                </div>{" "}
+              </div>
+              <div>
+                {" "}
+                <div>Delivery Charge</div>{" "}
+                <div>
+                  <span className="greenspan">Free</span>
+                </div>{" "}
+              </div>
+              <div>
+                {" "}
+                <div>Total amount</div>{" "}
+                <div>{(totalAmount - (totalAmount * 20) / 100).toFixed(2)}</div>{" "}
+              </div>
+            </div>
+            <div className="place_order_btn">
+              {" "}
+              <Button colorScheme="blue">Place Order</Button>
+            </div>
+          </Box>
+        </Box>
       </Box>
     </div>
   );
